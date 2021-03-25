5d712686190b6993b204e5b01a8e1b3ed2126e8f
Yosshua Cisneros <42878951+YOSSHUA@users.noreply.github.com>
2021-03-25 16:41:47 -0600

    Update motorPot.ino

--- pratice4/motorPot.ino
+++ pratice4/motorPot.ino
@@ -1,28 +1,28 @@
-int EN = 10; 
-
+const int EN = 10; 
+double v;
 void setup()
 {
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(EN, OUTPUT);
   Serial.begin(9600);
-  
+
 }
 
-double v;
+
 void loop()
 {
-  v = analogRead(A0);
-  v = map(v, 0, 1023, -255, 255);
+  v = map(analogRead(A0), 0, 1023, -255, 255);
   analogWrite(EN, abs(v));
   Serial.println(v);
-  
+
   if(v > 0){
     digitalWrite(12, LOW);
     digitalWrite(11, HIGH);
-  }else if (v < 0){
+  }else if(v < 0){
     digitalWrite(11, LOW);
     digitalWrite(12, HIGH);
   }
-  
+
+
 }
